56179a1643a7edab6a6cc9fc80951705607b6e3a
Tim Crundall <tim.crundall@gmail.com>
2016-05-14 18:14:42 +1000

    1-D Histograms working

--- ass5_4.py
+++ ass5_4.py
@@ -1,27 +1,36 @@
 import math
+import numpy as np
+import matplotlib.mlab as mlab
+import matplotlib.pyplot as plt
+import pylab as pl
 
 # Constants:
 R0 = 10 #kpc # distance from sun to galactic centre in parsecs (approx)
+V0 = 250
 
 def vel(r, l):
-    R = math.sqrt(r**2 + R0**2 + r*R0*math.cos(l)) #kpc
+    R = math.sqrt(r**2 + R0**2 - 2*r*R0*math.cos(l)) #kpc
+    #print "For r: " + str(r) + " and angle: " + str(l) + \
+    #       " R is: " + str(R)
+
     if R < 5 and R > 0:
         V = 50*R #km/s
     elif R >= 5:
         V = 250 #km/s
-    else:
-        print "oops.... shouldn't make it to this else"
 
-    return -R0 * V * (r + R0 * math.cos(l)) * math.sin(l)             \
-            /(r**2 + R0**2 + 2 * r * R0 * math.cos(l)**(3./2))
+    return R0*(V / R - V0 / R0)*math.sin(l)
 
 def main():
-    print vel(10, 0)
-    print vel(5, 0)
-    print
-    print vel(5,math.pi/2)
-    print vel(2,math.pi/4)
+    l = math.pi/2
+    x = np.linspace(0,20,100)
+    y = [vel(r, l) for r in x]
+    
 
+    n, bins, patches = plt.hist(y, 30)
+
+    print bins
+
+    plt.show()
 
 main()
 
