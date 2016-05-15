63f28e605acdec5fd0bfe804925d97a59833bcc0
Tim Crundall <tim.crundall@gmail.com>
2016-05-15 15:21:30 +1000

    Q4 done

--- q4.py
+++ q4.py
@@ -9,55 +9,6 @@ import matplotlib.cm as cm
 R0 = 10 #kpc # distance from sun to galactic centre in parsecs (approx)
 V0 = 250
 
-def vel(r, l):
-    R = math.sqrt(r**2 + R0**2 - 2*r*R0*math.cos(l)) #kpc
-    #print "For r: " + str(r) + " and angle: " + str(l) + \
-    #       " R is: " + str(R)
-
-    if R < 5 and R >= 0:
-        V = 50*R #km/s
-    elif R >= 5:
-        V = 250 #km/s
-
-    return R0*(V / R - V0 / R0)*math.sin(l)
-
-def vel2(r, l):
-    R = math.sqrt(r**2 + R0**2 - 2*r*R0*math.cos(l)) #kpc
-    #print "For r: " + str(r) + " and angle: " + str(l) + \
-    #       " R is: " + str(R)
-
-    if R < 5 and R >= 0:
-        V = 50*R #km/s
-    elif R >= 5:
-        V = 250 #km/s
-
-    if R > 4 and R < 6:
-        bright = True
-    else:
-        bright = False
-
-    return R0*(V / R - V0 / R0)*math.sin(l), bright
-
-
-def vel3(r, l):
-    R = math.sqrt(r**2 + R0**2 - 2*r*R0*math.cos(l)) #kpc
-    #print "For r: " + str(r) + " and angle: " + str(l) + \
-    #       " R is: " + str(R)
-
-    if R < 5 and R >= 0:
-        V = 50*R #km/s
-    elif R >= 5:
-        V = 250 #km/s
-
-    if R > 4 and R < 6:
-        bright = True
-        vel_rad = 50*math.cos( math.asin( math.sin(l)*R/R0 ) )
-    else:
-        vel_rad = 0
-        bright = False
-
-    return R0*(V / R - V0 / R0)*math.sin(l) - vel_rad, bright
-
 def vel4(r, l, R):
     #print "For r: " + str(r) + " and angle: " + str(l) + \
     #       " R is: " + str(R)
@@ -69,25 +20,9 @@ def vel4(r, l, R):
 
     return R0*(V / R - V0 / R0)*math.sin(l)
 
-def vel5(r, l, R):
-    #print "For r: " + str(r) + " and angle: " + str(l) + \
-    #       " R is: " + str(R)
-
-    if R < 5 and R >= 0:
-        V = 50*R #km/s
-    elif R >= 5:
-        V = 250 #km/s
-
-    if R < 6 and R > 4:
-        duplicate = True
-    else:
-        duplicate = False
-
-    return R0*(V / R - V0 / R0)*math.sin(l), duplicate
-
 def partB():
-    ls = np.linspace(0.001, math.pi/2, 400)
-    rs = np.linspace(0.001, 25, 400)
+    ls = np.linspace(0.001, math.pi/2, DATA_DIM)
+    rs = np.linspace(0.001, 25, DATA_DIM)
     
     l_list = []
     vel_list = []
@@ -99,12 +34,12 @@ def partB():
                 l_list.append(ls[i])
                 vel_list.append(vel4(rs[j], ls[i], R))
 
-    plt.hist2d(l_list, vel_list, 50, cmin=1)
+    plt.hist2d(l_list, vel_list, PLOT_DIM, cmin=1)
     plt.savefig("partb.png")
             
 def partC():
-    ls = np.linspace(0.001, math.pi/2, 400)
-    rs = np.linspace(0.001, 25, 400)
+    ls = np.linspace(0.001, math.pi/2, DATA_DIM)
+    rs = np.linspace(0.001, 25, DATA_DIM)
     
     l_list = []
     vel_list = []
@@ -119,8 +54,37 @@ def partC():
                     l_list.append(ls[i])
                     vel_list.append(vel4(rs[j], ls[i], R))
 
-    plt.hist2d(l_list, vel_list, 50, cmin=1)
+    plt.hist2d(l_list, vel_list, PLOT_DIM, cmin=1)
     plt.savefig("partc.png")
+
+def partD():
+    ls = np.linspace(-math.pi/2, math.pi/2, DATA_DIM*2)
+    rs = np.linspace(0.001, 25, DATA_DIM)
+    
+    l_list = []
+    vel_list = []
+
+    for i in range(len(ls)):
+        for j in range(len(rs)):
+            R = math.sqrt(rs[j]**2 + 10**2 - 2 * 10 * rs[j] * math.cos(ls[i]))
+            if (R < 15):
+                if (R > 4 and R < 6):
+                    vel_add = 50*math.cos( math.asin( 10*(math.sin(abs(ls[i]))/R )) )
+                    #vel_add = 0 #subbing out radial expansion
+                    vel = vel4(rs[j], ls[i], R) - vel_add*(ls[i]/abs(ls[i]))
+                    l_list.append(ls[i])
+                    l_list.append(ls[i])
+                    vel_list.append(vel)
+                    vel_list.append(vel)
+                else:
+                    l_list.append(ls[i])
+                    vel_list.append(vel4(rs[j], ls[i], R))
+
+    plt.hist2d(l_list, vel_list, PLOT_DIM, cmin=1)
+    plt.savefig("partd.png")
  
-partB()
-partC()
+DATA_DIM = 500
+PLOT_DIM = 300
+#partB()
+#partC()
+partD()
