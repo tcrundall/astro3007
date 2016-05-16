cde0b9c15f00974eacd8a596dfb1320f42dd9205
Tim Crundall <tim.crundall@gmail.com>
2016-05-16 21:42:49 +1000

    q3 part 1 done

--- q3.py
+++ q3.py
@@ -9,13 +9,51 @@ def tau(v):
     Ts = 150
     return 5.49*10e-19*NH/(Ts*math.sqrt(2*math.pi*omega)) \
                 *math.exp(-v**2/(2*omega**2))
-vs = np.linspace(-100, 100, 100)
+
+def Tb(v):
+    Ts = 150
+    return Ts*(1 - math.exp(-tau(v)))
+
+def gaus(x):
+    norm = 1/(omega_prime*math.sqrt(2*math.pi))
+
+    return norm*math.exp(-x**2/(2*omega_prime**2))
+
+vs = np.linspace(-5* omega,5* omega, 500)
 
 NHs = np.linspace(5e19, 5e20, 10)
 
 for NH in NHs:
 
-    ys = [tau(v) for v in vs]
+    ys = [Tb(v) for v in vs]
+    peak = max(ys)
+
+    i = 0
+    while (ys[i] < 0.5*peak):
+        i += 1
+    FWHM = -2*vs[i]
+
+    omega_prime = FWHM/2.355
+
+    zs = [gaus(x) for x in vs]
+    gaus_peak = max(zs)
+    #print gaus_peak
+
+    zs = [z*peak/gaus_peak for z in zs]
+
+    difference = 0
+    total = 0
+    for i in range(len(vs)):
+        difference = difference + abs(zs[i] - ys[i])
+        total = total + (ys[i])
+
+    print difference/total * 100
+    
+
 
     plt.plot(vs, ys)
-plt.show()
+    plt.plot(vs, zs, '--')
+
+    
+
+plt.savefig("q3.png")
