94f03bd1a5ab8bde3b23ee42ca4256babc8f1d38
Tim Crundall <tim.crundall@gmail.com>
2016-05-16 22:07:44 +1000

    q3 part 2 done

--- q3.py
+++ q3.py
@@ -1,4 +1,5 @@
 import math
+import random    
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -14,20 +15,58 @@ def Tb(v):
     Ts = 150
     return Ts*(1 - math.exp(-tau(v)))
 
-def gaus(x):
+def gaus(x, omega_prime):
     norm = 1/(omega_prime*math.sqrt(2*math.pi))
 
     return norm*math.exp(-x**2/(2*omega_prime**2))
 
-vs = np.linspace(-5* omega,5* omega, 500)
+def main():
+    vs = np.linspace(-5* omega,5* omega, 500)
 
-NHs = np.linspace(5e19, 5e20, 10)
+    NHs = np.linspace(5e19, 5e20, 10)
 
-for NH in NHs:
+    for NH in NHs:
+
+        ys = [Tb(v) for v in vs]
+        peak = max(ys)
+
+        i = 0
+        while (ys[i] < 0.5*peak):
+            i += 1
+        FWHM = -2*vs[i]
+
+        omega_prime = FWHM/2.355
+
+        zs = [gaus(x, omega_prime) for x in vs]
+        gaus_peak = max(zs)
+        #print gaus_peak
+
+        zs = [z*peak/gaus_peak for z in zs]
+
+        difference = 0
+        total = 0
+        for i in range(len(vs)):
+            difference = difference + abs(zs[i] - ys[i])
+            total = total + (ys[i])
+
+        print str(NH) + " resulted with a difference of " + \
+                str(difference/total * 100) + "%"
+        
+
+
+        plt.plot(vs, ys)
+        plt.plot(vs, zs, '--')
+    plt.savefig("q3.png")
+
+def main2():
+    noise = 3
+    
+    vs = np.linspace(-5*omega, 5*omega,500)
 
     ys = [Tb(v) for v in vs]
-    peak = max(ys)
+    rs = [Tb(v)+random.randrange(-noise, noise) for v in vs]
 
+    peak = max(ys)
     i = 0
     while (ys[i] < 0.5*peak):
         i += 1
@@ -35,25 +74,38 @@ for NH in NHs:
 
     omega_prime = FWHM/2.355
 
-    zs = [gaus(x) for x in vs]
+    zs = [gaus(x, omega_prime) for x in vs]
     gaus_peak = max(zs)
-    #print gaus_peak
 
     zs = [z*peak/gaus_peak for z in zs]
 
     difference = 0
     total = 0
     for i in range(len(vs)):
-        difference = difference + abs(zs[i] - ys[i])
-        total = total + (ys[i])
+        difference = difference + abs(zs[i] - rs[i])
+        total = total + zs[i]
 
-    print difference/total * 100
-    
+    print "NH is: " + str(NH)
+    print "Noise is: " + str(noise)
 
+    print "Difference of noise with gaus: " + str(difference/total*100)
+
+    difference = 0
+    total = 0
+    for i in range(len(vs)):
+        difference = difference + abs(ys[i] - rs[i])
+        total = total + ys[i]
+
+    print "Difference of noise with true: " + str(difference/total*100)
 
     plt.plot(vs, ys)
-    plt.plot(vs, zs, '--')
+    plt.plot(vs, rs)
+    plt.plot(vs,zs, '--')
 
+    plt.savefig("q3_part2.png")
+
+NH = 2.5e20
+main2()
     
 
-plt.savefig("q3.png")
+
