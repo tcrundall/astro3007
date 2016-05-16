63fb9a7a45cf82b7694898813144aed3d097090e
Tim Crundall <tim.crundall@gmail.com>
2016-05-16 22:13:16 +1000

    With resutls

--- q3.py
+++ q3.py
@@ -20,43 +20,43 @@ def gaus(x, omega_prime):
 
     return norm*math.exp(-x**2/(2*omega_prime**2))
 
-def main():
-    vs = np.linspace(-5* omega,5* omega, 500)
+#def main():
+vs = np.linspace(-5* omega,5* omega, 500)
 
-    NHs = np.linspace(5e19, 5e20, 10)
+NHs = np.linspace(5e19, 5e20, 10)
 
-    for NH in NHs:
+for NH in NHs:
 
-        ys = [Tb(v) for v in vs]
-        peak = max(ys)
+    ys = [Tb(v) for v in vs]
+    peak = max(ys)
 
-        i = 0
-        while (ys[i] < 0.5*peak):
-            i += 1
-        FWHM = -2*vs[i]
+    i = 0
+    while (ys[i] < 0.5*peak):
+        i += 1
+    FWHM = -2*vs[i]
 
-        omega_prime = FWHM/2.355
+    omega_prime = FWHM/2.355
 
-        zs = [gaus(x, omega_prime) for x in vs]
-        gaus_peak = max(zs)
-        #print gaus_peak
+    zs = [gaus(x, omega_prime) for x in vs]
+    gaus_peak = max(zs)
+    #print gaus_peak
 
-        zs = [z*peak/gaus_peak for z in zs]
+    zs = [z*peak/gaus_peak for z in zs]
 
-        difference = 0
-        total = 0
-        for i in range(len(vs)):
-            difference = difference + abs(zs[i] - ys[i])
-            total = total + (ys[i])
+    difference = 0
+    total = 0
+    for i in range(len(vs)):
+        difference = difference + abs(zs[i] - ys[i])
+        total = total + (ys[i])
 
-        print str(NH) + " resulted with a difference of " + \
-                str(difference/total * 100) + "%"
-        
+    print str(NH) + " resulted with a difference of " + \
+            str(difference/total * 100) + "%"
+    
 
 
-        plt.plot(vs, ys)
-        plt.plot(vs, zs, '--')
-    plt.savefig("q3.png")
+    plt.plot(vs, ys)
+    plt.plot(vs, zs, '--')
+plt.savefig("q3.png")
 
 def main2():
     noise = 3
@@ -105,7 +105,7 @@ def main2():
     plt.savefig("q3_part2.png")
 
 NH = 2.5e20
-main2()
+#main2()
     
 
 
